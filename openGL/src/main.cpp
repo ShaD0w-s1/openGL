d5b63cb094e673fae636f2abfee34a09a02ae592
zhangyuxuan <zhangyuxuan01@gmail.com>
2022-05-22 22:54:47 +0800

    none

--- openGL/src/main.cpp
+++ openGL/src/main.cpp
@@ -1,48 +1,78 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+
+ }
+};
+
 int main(void)
 {
-  GLFWwindow* window;
+	GLFWwindow* window;
 
-  /* Initialize the library */
-  if (!glfwInit())
-    return -1;
 
-  glewInit();
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window)
-  {
-    glfwTerminate();
-    return -1;
-  }
 
-  /* Make the window's context current */
-  glfwMakeContextCurrent(window);
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-  /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window))
-  {
-    /* Render here */
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glVertex2f(-0.5f, 0.5f);
 
-    glEnd();
+	/* Make the window's context current */
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    /* Swap front and back buffers */
-    glfwSwapBuffers(window);
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
-    /* Poll for and process events */
-    glfwPollEvents();
-  }
+	float postions[6] = {
+			-0.5f, -0.5f,
+			 0.5f, -0.5f,
+			-0.5f,  0.5f
+	};
 
-  glfwTerminate();
-  return 0;
+	unsigned int buffer;
+
+	glGenBuffers(1, &buffer);
+
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), postions,GL_STATIC_DRAW);
+
+
+
+
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glBegin(GL_TRIANGLES);
+
+			glDrawArrays(GL_TRIANGLES,);
+
+			glEnd();
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+
+	glfwTerminate();
+	return 0;
 }
