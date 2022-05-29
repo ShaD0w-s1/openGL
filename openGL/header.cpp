e035c5415bf88c2d098e5bb4c1099efd3d655dc2
zhangyuxuan <zhangyuxuan01@gmail.com>
2022-05-29 17:14:53 +0800

    抽离着色器重复逻辑

---
+++ openGL/header.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <GL/glew.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+	public:
+		unsigned int ID;
+		Shader() {
+
+		}
+};
--- openGL/src/main.cpp
+++ openGL/src/main.cpp
@@ -20,6 +20,74 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
+
+
+static unsigned int CompileShader(unsigned int type ,const char* shaderSource)
+{ 
+	int success;
+	char infoLog[512];
+
+	// 定义着色器类型并指定ID
+	unsigned int shader = glCreateShader(type);
+
+	// 确定着色器源码指针
+	glShaderSource(shader, 1, &shaderSource, nullptr);
+
+	// 执行编译
+	glCompileShader(shader);
+
+	// 确定着色器状态
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::"<< ( type == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT" ) << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+		return -1;
+
+		// TODO someAnthorErrorHandle
+	}
+
+	return shader;
+};
+
+static unsigned int CreateShader(const char* vertexShaderSource, const char* fragmentShaderSource)
+{
+	int success;
+	char infoLog[512];
+
+	// 指定子程序ID
+	unsigned int shaderProgram = glCreateProgram();
+
+	// 
+	unsigned int VS = CompileShader(GL_VERTEX_SHADER, vertexShaderSource);
+	unsigned int FS = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+	// 
+	glAttachShader(shaderProgram, VS);
+	glAttachShader(shaderProgram, FS);
+	glLinkProgram(shaderProgram);
+
+	// 确定子程序状态
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+
+		return -1;
+
+		// TODO someAnthorErrorHandle
+	}
+
+	// 清空内存( 建议不删 方便调试？ ) 
+	glDeleteShader(VS);
+	glDeleteShader(FS);
+
+	return shaderProgram;
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	// make sure the viewport matches the new window dimensions; note that width and 
@@ -54,50 +122,18 @@ int main()
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// 初始化glew
+	// 初始化GL函数指针
 	glewInit();
 
-	// 顶点着色器
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	// 编译错误捕获
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	// 片段着色器
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	// 编译错误捕获
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
 
-	// 链接着色器
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	// 链接错误捕获
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+
+	unsigned int shaderProgram = CreateShader(vertexShaderSource, fragmentShaderSource);
+
 
 	// 设置顶点
 	float vertices[] = {
@@ -113,23 +149,27 @@ int main()
 			1, 2, 3  // 第二个三角形
 	};
 
+	// 创建显存缓冲
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
 
+	// 绑定顶点数组对象
 	glBindVertexArray(VAO);
 
+	// 绑定顶点缓冲对象
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// 绑定索引缓冲对象
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
 
